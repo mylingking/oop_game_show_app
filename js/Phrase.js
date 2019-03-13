957dbd2fc7196e703662b07fa4cf6d6a5aacbd8e
Ran Wang <wang.ran.henri@gmail.com>
2019-03-13 06:49:37 -0400

    error addPhraseToDisplay method

--- js/Phrase.js
+++ js/Phrase.js
@@ -7,8 +7,29 @@ class Phrase {
      * display phrase on game board
      */
     addPhraseToDisplay() {
+        // create an array of splited letters from the random phrase
         const letters = [];
         letters.push(this.phrase.split(''));
-        return letters;
+        //create the html of the splited letters
+        const div = document.createElement('div');
+        const PhraseHTML = document.createElement('div');
+        PhraseHTML.className = 'section';
+        PhraseHTML.id = 'phrase';
+        const ul = document.createElement('ul');
+        //loop to add li elements to ul
+        letters.forEach(letter => {
+            const li = document.createElement('li');
+            if (letter === ' ') {
+                li.className = 'space';
+                li.innerHTML = ' ';
+                console.log(li);
+            } else {
+                li.classList.add('hide', 'letter', letter);
+                li.innerHTML = letter;
+            };
+            ul.appendChild(li);
+        });
+
+        console.log(phraseHTML);
     };
 };
